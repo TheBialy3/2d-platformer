1205b534ff97f05a7753f7c348b3a991d9f3cff0
Karol Białas <107372713+TheBialy3@users.noreply.github.com>
2025-12-02 22:30:21 +0100

    bug fix

--- Scripts/end_flag.gd
+++ Scripts/end_flag.gd
@@ -4,5 +4,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
 		return
-	
+	call_deferred("next_lvl")
+func next_lvl():
 	get_tree().change_scene_to_packed(scene_to_load)
